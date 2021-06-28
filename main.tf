823a8f781937ad621cbc8498621ea592c6ae9148
Bruno Santos <bruno.santos@zoop.com.br>
2021-06-28 07:54:58 -0300

    FMT

--- main.tf
+++ main.tf
@@ -50,7 +50,3 @@ resource "aws_instance" "test" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http.id]
 }
-
-output "ip" {
-  value = aws_instance.test.public_id
-}
